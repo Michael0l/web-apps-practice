9a10e081cc6d8258ad29ee762bf4b5a17619211a
Michael0l <40627547+Michael0l@users.noreply.github.com>
2022-06-08 23:35:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "5310972503:AAEv5Icx3KgUbXJqvgzewjOnIAeyrDoA4bg"
 
-WEBHOOK_HOST = f'https://web-apps-practice.herokuapp.com'
+WEBHOOK_HOST = f'web-apps-practice.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
