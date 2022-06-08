7281781295d047454d5e16802bf0e0032027a230
Michael0l <40627547+Michael0l@users.noreply.github.com>
2022-06-08 23:31:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 BOT_TOKEN = "5310972503:AAEv5Icx3KgUbXJqvgzewjOnIAeyrDoA4bg"
-WEBHOOK_HOST = "git.heroku.com"
-WEBHOOK_PATH = "/web-apps-practice.git"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBHOOK_HOST = f'https://web-apps-practice.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
 WEBAPP_HOST = "0.0.0.0"
 WEBAPP_PORT = 8000
