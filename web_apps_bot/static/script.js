817530cde79facdfe7aa5c132886d587efa35561
Michael0l <40627547+Michael0l@users.noreply.github.com>
2022-06-09 15:17:05 +0300

    Add files via upload

---
+++ web_apps_bot/static/script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', getFormValue);
+
+
+Telegram.WebApp.ready()
+configureThemeColor(Telegram.WebApp.colorScheme);
+
+
+
+
+function getFormValue(event) {
+  event.preventDefault();
+  var name = form.querySelector('[name="name"]'),
+    email = form.querySelector('[name="email"]'),
+    text = form.querySelector('[name="text"]')
+    var data = {
+    name: name.value,
+    email: email.value,
+    text: text.value
+  };
+  if (name.value === '' || email.value === '' || text.value === '') {
+    for (key in data) {
+      el = form.querySelector(`[name="${key}"]`);
+      if (el.value === '') {
+        el.style.borderColor = '#CC4949';
+      } else {
+        el.style.borderColor = '#408f04';
+      };
+    };
+  } else {
+      for (key in data) {
+        el = form.querySelector(`[name="${key}"]`);
+        el.style.borderColor = '#00677e';
+        };
+      name.value = ''
+      email.value = ''
+      text.value = ''
+      console.log(data) 
+    };
+};
+
+
