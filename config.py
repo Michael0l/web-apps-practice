1db57ba5b81ba8eca75a59f32268d70559996a4f
Michael0l <40627547+Michael0l@users.noreply.github.com>
2022-06-09 15:15:59 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "5310972503:AAEv5Icx3KgUbXJqvgzewjOnIAeyrDoA4bg"
-WEBHOOK_HOST = f'web-apps-practice.herokuapp.com'
-WEBHOOK_PATH = "/web_app_example"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = 8000
